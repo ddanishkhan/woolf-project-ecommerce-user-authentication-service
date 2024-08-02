36cc6196d27f22ae4b8a1c619a4dd44915c9de34
Danish Khan <danissh20@gmail.com>
2024-08-02 23:16:54 +0530

    Skeleton of project

---
+++ src/main/java/com/ecommerce_user_authentication/EcommerceUserAuthenticationApplication.java
@@ -0,0 +1,13 @@
+package com.ecommerce_user_authentication;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EcommerceUserAuthenticationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EcommerceUserAuthenticationApplication.class, args);
+	}
+
+}
