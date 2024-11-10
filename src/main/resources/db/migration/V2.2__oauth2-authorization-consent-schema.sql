12cdfd8d2434ef12ac269394b417417d9c0ae6bb
Danish Khan <danissh20@gmail.com>
2024-11-10 12:21:38 +0530

    Revert "Added flyway for storing oauth client data."
    This reverts commit cc0c0f59320fec455838d581aefe6207978f6b43.

--- src/main/resources/db/migration/V2.2__oauth2-authorization-consent-schema.sql
+++
@@ -1,8 +0,0 @@
--- Picked from Spring Repo itself.
--- https://github.com/spring-projects/spring-authorization-server/tree/main/oauth2-authorization-server/src/main/resources/org/springframework/security/oauth2/server/authorization
-CREATE TABLE oauth2_authorization_consent (
-    registered_client_id varchar(100) NOT NULL,
-    principal_name varchar(200) NOT NULL,
-    authorities varchar(1000) NOT NULL,
-    PRIMARY KEY (registered_client_id, principal_name)
-);
